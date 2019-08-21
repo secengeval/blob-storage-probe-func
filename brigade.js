168b9b3515d7e5b6216ec721455fd7730720dd97
Adriaan Stander <Adriaan.Stander@dentsuaegis.com>
2019-08-21 08:25:28 +0200

    Cleaning up with brigade-testing

--- brigade.js
+++ brigade.js
@@ -1,16 +1,10 @@
 const { Job, Group } = require("brigadier")
 const devops = require("devops-brigade");
 
-// TODO: The notifyInfoAsync method requires a Microsoft Teams webhook - see
-// https://confluence-engineering.dentsuaegis.com/display/GD/Send+notifications+to+Teams+channel
-// or comment them out until ready
-
 class JobFactory {
   createBuildJob(e, project) {
-    // TODO: If not "node", specify alternative docker container for your build
-    var build = new Job("build", "node") 
+    var build = new Job("build", "node:10.16.3") 
     build.storage.enabled = true
-
     let taskFactory = new devops.BuildTaskFactory(e, project)
     build.tasks = [
       "cd /src",
@@ -20,52 +14,32 @@ class JobFactory {
       "git config --global credential.helper 'store'",
       "rm -rf node_modules",
       "npm install",
-      `zip ${project.secrets.app_name}.zip *`
+      `zip ${project.secrets.app_name}.zip *`,
       taskFactory.storeBuild()
     ]
-
     return build;
   }
+ 
 
   createDeployJob(teamEnv, e, project) {
     let deployTaskFactory = new devops.DeployTaskFactory(teamEnv, e, project)
     let deployJob = new Job(`deploy-${teamEnv}`, `globaldevopsreg11.azurecr.io/builder:latest`)
     deployJob.storage.enabled = true
 
-    // TODO: Uncomment this section if we are gonna use kubernetes as our compute 
-    // let values = {
-    //   image: {
-    //     tag: "${APP_VER}",
-    //     repository: `${project.secrets.app_container_reg}/${devops.Utilities.getAppName()}`
-    //   }
-    // };
-
     deployJob.tasks = [
-      // If doing a cluster type deployment then uncomment this
-      // deployTaskFactory.loginToCluster(),
       deployTaskFactory.setAppVerEnv(),
       "cd /src/",
-      `az functionapp deployment source config-zip  -g {project.secrets.azure_resource_group} -n ${project.secrets.app_name} --src ./${project.secrets.app_name}.zip`
-      // deployTaskFactory.helmUpgradeInstallCommandWithValidation(
-      //   `${teamEnv}`,
-      //   `${teamEnv}-${project.secrets.app_name}`,
-      //   `./helm/${project.secrets.app_name}`,
-      //   values)
+      `az functionapp deployment source config-zip  -g {project.secrets.azure_resource_group} -n ${project.secrets.app_name} --src ./${project.secrets.app_name}.zip`,
     ]
-
     return deployJob;
   }
 }
 
-// TODO: notification via teams on error - remove if not using teams
-devops.Events.enableNotifyOnError();
-
 devops.Events.onPushDevelop(async (e, project) => {
   let jobFactory = new JobFactory();
   await jobFactory.createBuildJob(e, project).run();
-  // TODO: add SonarQube integration https://confluence-engineering.dentsuaegis.com/display/GD/Sonarqube
   await jobFactory.createDeployJob(`${project.secrets.team_name}-dev`, e, project).run();
-  //var semver = await devops.Utilities.getSemVerAsync();
+  await devops.Utilities.notifyInfoAsync(`Deployment to test complete`, `Deployed version ${semver}`);
 });
 
 devops.Events.onPushOther(async (e, project) => {
